43aebcb9d7361ecd879f7a26e5ae629d0a3d2b56
samsuite <suitester@gmail.com>
2017-02-22 23:53:46 -0500

    circle fill stuff

--- Assets/Scripts/Frontend/linerenderer_circle.cs
+++ Assets/Scripts/Frontend/linerenderer_circle.cs
@@ -10,7 +10,8 @@ public class linerenderer_circle : MonoBehaviour {
     public Vector2 center = Vector2.zero;
 
     LineRenderer lines;
-	
+	public LineRenderer fill;
+
 	void Start () {
         lines = GetComponent<LineRenderer>();
 
@@ -31,11 +32,16 @@ public class linerenderer_circle : MonoBehaviour {
 
     void redraw_circle () {
         lines.numPositions = num_points+2;
+        fill.numPositions = num_points+2;
+
+        fill.startWidth = radius + 0.1f;
+        fill.endWidth = radius + 0.1f;
+
 
         for (int i = 0; i < num_points+2; i++){
             float theta = ((float)i/(float)num_points)*Mathf.PI*2;
             lines.SetPosition(i, transform.position + new Vector3(Mathf.Cos(theta)*radius,Mathf.Sin(theta)*radius,0f));
+            fill.SetPosition(i, transform.position + new Vector3(Mathf.Cos(theta)*(radius/2f - 0.1f),Mathf.Sin(theta)*(radius/2f - 0.1f),0f));
         }
     }
-
 }
