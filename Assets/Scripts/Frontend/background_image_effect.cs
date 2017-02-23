a071e5228c722245e16298f298fffa84d2d7e89e
samsuite <suitester@gmail.com>
2017-02-23 12:51:20 -0500

    antialiasing, rendertexture updates scale when scene is resized

--- Assets/Scripts/Frontend/background_image_effect.cs
+++ Assets/Scripts/Frontend/background_image_effect.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class background_image_effect : MonoBehaviour {
 
+    int res_w;
+    int res_h;
+
 	void Start () {
 		
         RenderTexture prepass = new RenderTexture(Screen.width,Screen.height,24);
@@ -14,6 +17,35 @@ public class background_image_effect : MonoBehaviour {
         camera.targetTexture = prepass;
         camera.SetReplacementShader(fill_shader, "RenderType");
         Shader.SetGlobalTexture("_FillPrepassTex", prepass);
+
+        res_w = Screen.width;
+        res_h = Screen.height;
 	}
+
+
+    void Update () {
+
+        if (res_w != Screen.width || res_h != Screen.height) {
+            print ("screen size changed");
+            res_w = Screen.width;
+            res_h = Screen.height;
+
+            // clear the rendertexture
+            Camera camera = GetComponent<Camera>();
+            if (camera.targetTexture != null) {
+                camera.targetTexture.Release();
+            }
+
+            // make a new one
+            RenderTexture prepass = new RenderTexture(Screen.width,Screen.height,24);
+            Shader fill_shader = Shader.Find("Hidden/Fill Replace");
+
+            camera.targetTexture = prepass;
+            camera.SetReplacementShader(fill_shader, "RenderType");
+            Shader.SetGlobalTexture("_FillPrepassTex", prepass);
+             
+        }
+    }
+
 
 }
