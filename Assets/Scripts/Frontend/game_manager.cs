bceb8b6116ec565367c4fff64605d33b3e75ef60
samsuite <suitester@gmail.com>
2017-04-07 14:04:07 -0400

    executable attempt

---
+++ Assets/Scripts/Frontend/game_manager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_manager : MonoBehaviour {
+
+	public void quit_game ()
+    {
+        Application.Quit();
+    }
+}
