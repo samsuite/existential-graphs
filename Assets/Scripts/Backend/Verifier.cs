c223ec443712076e39b5761d6b6b9675e3936a6c
Christian Encarnacion <encarc@rpi.edu>
2017-04-21 14:05:01 -0400

    more stuff

--- Assets/Scripts/Backend/Verifier.cs
+++ Assets/Scripts/Backend/Verifier.cs
@@ -20,19 +20,28 @@ public class Verifier : MonoBehaviour
         AlphaChecker c = new AlphaChecker();
         if (!par.prev_state.Equals(par.curr_state))
         {
-            Pair<ExistentialGraph, ExistentialGraph> difp = c.Find_Difference_Between(par.prev_state, par.curr_state);
-            if (c.Is_Double_Cut(difp.item1, difp.item2))
-                Debug.Log("It's a Double Cut!");
-            else if (c.Is_Iteration(difp.item1, difp.item2))
-                Debug.Log("It's Iteration!");
-            else if (c.Is_Deiteration(difp.item1, difp.item2))
-                Debug.Log("It's Deiteration!");
-            else if (c.Is_Insertion(difp.item1, difp.item2))
-                Debug.Log("It's Insertion!");
-            else if (c.Is_Erasure(difp.item1, difp.item2))
-                Debug.Log("It's Erasure!");
-        }
+            try
+            {
+                Pair<ExistentialGraph, ExistentialGraph> difp = c.Find_Difference_Between(par.prev_state, par.curr_state);
+                if (c.Is_Double_Cut(difp.item1, difp.item2))
+                    Debug.Log("It's a Double Cut!");
+                else if (c.Is_Iteration(difp.item1, difp.item2))
+                    Debug.Log("It's Iteration!");
+                else if (c.Is_Deiteration(difp.item1, difp.item2))
+                    Debug.Log("It's Deiteration!");
+                else if (c.Is_Insertion(difp.item1, difp.item2))
+                    Debug.Log("It's Insertion!");
+                else if (c.Is_Erasure(difp.item1, difp.item2))
+                    Debug.Log("It's Erasure!");
+                else
+                    Debug.Log("No valid rule found!");
+            }
+            catch (Exception e)
+            {
+                return;
+            }
 
+        }
     }
 
     public void Done()
