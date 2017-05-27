506dc37a9a300cb84cd30d05419a6b4e7ee46455
Christian Encarnacion <encarc@rpi.edu>
2017-05-27 11:29:54 -0400

    Updated Output Messages in Verifier

--- Assets/Scripts/Backend/Verifier.cs
+++ Assets/Scripts/Backend/Verifier.cs
@@ -27,34 +27,34 @@ public class Verifier : MonoBehaviour
             {
                 Pair<ExistentialGraph, ExistentialGraph> difp = c.Find_Difference_Between(par.prev_state, par.curr_state);
 
-				if (c.A_DC_Exists(difp.item1, difp.item2)) {
+		if (c.A_DC_Exists(difp.item1, difp.item2)) {
                     Debug.Log("It's a Double Cut!");
-                    output_text.text = "It's a Double Cut!";
+                    output_text.text = "Double Cut";
                 }
                 else if (c.Is_Iteration(difp.item1, difp.item2)) {
                     Debug.Log("It's Iteration!");
-                    output_text.text = "It's Iteration!";
+                    output_text.text = "Iteration";
                 }
                 else if (c.Is_Deiteration(difp.item1, difp.item2)) {
                     Debug.Log("It's Deiteration!");
-                    output_text.text = "It's Deiteration!";
+                    output_text.text = "Deiteration";
                 }
                 else if (c.Is_Insertion(difp.item1, difp.item2)) {
                     Debug.Log("It's Insertion!");
-                    output_text.text = "It's Insertion!";
+                    output_text.text = "Insertion";
                 }
                 else if (c.Is_Erasure(difp.item1, difp.item2)) {
                     Debug.Log("It's Erasure!");
-                    output_text.text = "It's Erasure!";
+                    output_text.text = "Erasure";
                 }
                 else {
                     Debug.Log("No valid rule found!");
-                    output_text.text = "No valid rule found!";
+                    output_text.text = "No valid rule";
                 }
             }
             catch (Exception e)
             {
-				Debug.Log("EXCEPTION?" + e.ToString());
+		Debug.Log("EXCEPTION?" + e.ToString());
                 return;
             }
 
@@ -62,14 +62,14 @@ public class Verifier : MonoBehaviour
         else
         {
             Debug.Log("It's Identity!");
-            //output_text.text = "It's Identity!";
+            output_text.text = "Identity";
         }
     }
 
     public void Done()
     {
-        par.ConvertAndPrint();
-        print(par.prev_state.Equals(par.curr_state));
+        //par.ConvertAndPrint();
+	//We need a way of accessing the goal state
     }
 
 }
